5a1735fcba86a2af98d5506c167714e2934023c8
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-07-25 15:35:32 +0300

    getmovie

--- ProjectUI/Controllers/MovieController.cs
+++ ProjectUI/Controllers/MovieController.cs
@@ -28,6 +28,28 @@ namespace ProjectUI.Controllers
                 return View(apiMovieViewModels);
             }
         }
-       
+        public async Task<IActionResult> GetMovie(string topNumber)
+        {
+            ApiMovieViewModel model = new ApiMovieViewModel();
+            var client = new HttpClient();
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri($"https://imdb-top-100-movies.p.rapidapi.com/top1"),
+                Headers =
+    {
+        { "X-RapidAPI-Key", "8aeece6e7emshddc8f9b9159764dp18957fjsne809f1fcc81d" },
+        { "X-RapidAPI-Host", "imdb-top-100-movies.p.rapidapi.com" },
+    },
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                model = JsonConvert.DeserializeObject<ApiMovieViewModel>(body);
+                return View(model);
+            }
+
+        }
     }
 }
--- ProjectUI/Models/ApiMovieViewModel.cs
+++ ProjectUI/Models/ApiMovieViewModel.cs
@@ -3,10 +3,13 @@
     public class ApiMovieViewModel
     {
         public int rank { get; set; }
+        public string id { get; set; }
         public string title { get; set; }
         public string rating { get; set; }
         public string trailer { get; set; }
         public string thumbnail { get; set; }
+        public string year { get; set; }
+        public string description { get; set; }
         
     }
 }
