901a92183baedeb4e5a91d91064b1ef37584a7b7
ErayBayrak <47663033+ErayBayrak@users.noreply.github.com>
2023-07-25 17:29:13 +0300

    login

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,21 +1,43 @@
+
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+SecurityKey key = new SymmetricSecurityKey(System.Text.Encoding.ASCII.GetBytes("password"));
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+{
+	ValidateIssuer = true,
+	ValidateAudience = true,
+	ValidateLifetime = true,
+	ValidIssuer = "eray@eray.com",
+	ValidAudience = "eray@eray.com",
+	ValidateIssuerSigningKey = true,
+	IssuerSigningKey=key
+}
+);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
-
+app.UseAuthentication();
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
